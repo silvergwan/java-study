e585b2457664a25b2f03d83c6c978ddb97565e49
silvergwan <silvergwan@gmail.com>
2026-09-04 11:37:42 +0900

    feat: if, else if, else

---
+++ src/조건문/ipeu.java
@@ -0,0 +1,15 @@
+package 조건문;
+
+public class ipeu {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+    }
+}
